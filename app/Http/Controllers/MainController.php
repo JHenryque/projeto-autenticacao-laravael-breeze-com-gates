465a2a34d31afcab2746d2efcc5ee3f33f7069c5
JHenrique <henryque.pc.game@gmail.com>
2025-05-14 17:48:18 -0300

    Lógica No Controller Para Autorização com Gates

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Posts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 
@@ -15,5 +16,27 @@ class MainController extends Controller
         // get all posts and the data of the user who craete tje post
         $posts = Posts::with('user')->get();
         return view('dashboard', ['posts' => $posts]);
+    }
+
+    public function createPost()
+    {
+        // gate
+        if (Gate::denies('post.create'))
+        {
+            abort(403, 'Voçé nao tem permissão para criar novo posts.');
+        }
+
+        echo 'Create Post';
+    }
+
+    public function deletePost()
+    {
+        //gate
+        if (Gate::denies('post.delete'))
+        {
+            abort(403, 'Voçé nao tem permissão para deleta um post.');
+        }
+
+        echo 'Delete Post';
     }
 }
